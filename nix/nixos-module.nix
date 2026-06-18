efdd71c3fd54dae8ebe9717c9ad7186e2febeee2
skykanin <3789764+skykanin@users.noreply.github.com>
2026-06-18 21:35:58 +0200

    remove explicit PATH definition

--- nix/nixos-module.nix
+++ nix/nixos-module.nix
@@ -48,8 +48,6 @@ in
 
         serviceConfig = {
           ExecStart = lib.getExe cfg.package;
-          # Expose user and global packages to noctalia, speficially for the app launcher
-          # Environment = "PATH=/etc/profiles/per-user/%u/bin:/run/current-system/sw/bin";
           Restart = "on-failure";
         };
       };
