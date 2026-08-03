f02e6a82966c8bd859d84674ca70ba60ef59c143
Keanu Kerr <keanu@kerr.us>
2026-08-03 17:20:53 -0400

    feat(network): add cellular (ModemManager) support

--- src/shell/bar/bar.cpp
+++ src/shell/bar/bar.cpp
@@ -1432,6 +1432,8 @@ bool Bar::initialize(const BarServices& services) {
   m_sysmon = services.sysmon;
   m_powerProfiles = services.powerProfiles;
   m_network = services.network;
+  m_modem = services.modem;
+  m_externalIp = services.externalIp;
   m_idleInhibitor = services.idleInhibitor;
   m_mpris = services.mpris;
   m_audioSpectrum = services.audioSpectrum;
@@ -1483,6 +1485,8 @@ BarServices Bar::services() const {
       .sysmon = m_sysmon,
       .powerProfiles = m_powerProfiles,
       .network = m_network,
+      .modem = m_modem,
+      .externalIp = m_externalIp,
       .idleInhibitor = m_idleInhibitor,
       .mpris = m_mpris,
       .audioSpectrum = m_audioSpectrum,
