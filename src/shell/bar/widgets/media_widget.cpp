9fd9ea365548305c4319ff3d7a1ab00c6b846773
GhostEagle68 <ghosteagle68@gmail.com>
2026-08-06 00:05:46 -0600

    refactor(media): run just format and update null

--- src/shell/bar/widgets/media_widget.cpp
+++ src/shell/bar/widgets/media_widget.cpp
@@ -98,7 +98,11 @@ void MediaWidget::create() {
 
 void MediaWidget::doLayout(Renderer& renderer, float containerWidth, float containerHeight) {
   auto* rootNode = root();
-  if (rootNode == nullptr || m_art == nullptr || m_label == nullptr || m_emptyGlyph == nullptr || m_progressBar == nullptr) {
+  if (rootNode == nullptr
+      || m_art == nullptr
+      || m_label == nullptr
+      || m_emptyGlyph == nullptr
+      || m_progressBar == nullptr) {
     return;
   }
   syncState(renderer);
